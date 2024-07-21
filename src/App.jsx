650cbb3005ec546658bfe31b890b3df40ab56de1
Gabriel Soares <gabrielsoares.pe.br@gmail.com>
2024-07-20 22:41:55 -0300

    feat: Cabeçalho / Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import "./App.css";
+import logo from "./assets/images/logo.png";
 
 function App() {
   return (
     <>
-      <p className="text-red-400">Avante Consórcio</p>
+      <header>
+        <img src={logo} alt="Avante logotipo"/>
+
+        <ul id="redesSociais">
+          <li>
+            <a
+              href="https://api.whatsapp.com/send/?phone=558198477398&text&type=phone_number&app_absent=0"
+              target="_blank"
+            >
+              <i className="fa-brands fa-whatsapp"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com"
+              target="_blank"
+            >
+              <i className="fa-brands fa-instagram"></i>
+            </a>
+          </li>
+        </ul>
+      </header>
     </>
   );
 }
